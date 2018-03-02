b2e62ffa1187f2a1678d412f34061ec718d31eb7
Joshua Cox <jcox.pro@gmail.com>
2018-03-02 15:40:04 -0600

    breadcrumb comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,50 @@
 # Pages Controller
 class PagesController < ApplicationController
+  # Breadcrumb for
   # add_breadcrumb 'Home', :root_path
 
   def index
+    # Breadcrumb for
     # add_breadcrumb 'Dashboard', dashboard_path
   end
 
   def dashboard
+    # Breadcrumb for dashboard
     add_breadcrumb 'Dashboard', dashboard_path
   end
 
   def usermgmt
+    # Breadcrumb for User Management
     add_breadcrumb 'User Management', usermgmt_path
   end
 
   def customermgmt
+    # Breadcrumb for Customer Management
     add_breadcrumb 'Customer Management', customermgmt_path
   end
 
   def vendormgmt
+    # Breadcrumb for Vendor Management
     add_breadcrumb 'Vendor Management', vendormgmt_path
   end
 
   def ordermgmt
+    # Breadcrumb for Order Management
     add_breadcrumb 'Order Management', ordermgmt_path
   end
 
   def shippingmgmt
+    # Breadcrumb for Shipping Management
     add_breadcrumb 'Shipping Management', shippingmgmt_path
   end
 
   def reports
+    # Breadcrumb for Reports
     add_breadcrumb 'Reports', reports_path
   end
 
   def datatables
+    # Breadcrumb for Data Tables
     add_breadcrumb 'Data Tables', datatables_path
   end
 end
