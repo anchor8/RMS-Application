cfd32c1d32929c87d694f3d64ce945554cac56e4
Joshua Cox <jcox.pro@gmail.com>
2018-03-02 03:37:47 -0600

    updated breadcrumbs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,40 @@
 # Pages Controller
 class PagesController < ApplicationController
-  def index; end
+  add_breadcrumb 'Dashboard', :root_path
 
-  def dashboard; end
+  def index
+    add_breadcrumb 'Dashboard', dashboard_path
+  end
 
-  def usermgmt; end
+  def dashboard
+    add_breadcrumb 'Dashboard', dashboard_path
+  end
 
-  def customermgmt; end
+  def usermgmt
+    add_breadcrumb 'User Management', usermgmt_path
+  end
 
-  def vendormgmt; end
+  def customermgmt
+    add_breadcrumb 'Customer Management', customermgmt_path
+  end
 
-  def ordermgmt; end
+  def vendormgmt
+    add_breadcrumb 'Vendor Management', vendormgmt_path
+  end
 
-  def shippingmgmt; end
+  def ordermgmt
+    add_breadcrumb 'Order Management', ordermgmt_path
+  end
 
-  def reports; end
+  def shippingmgmt
+    add_breadcrumb 'Shipping Management', shippingmgmt_path
+  end
 
-  def datatables; end
+  def reports
+    add_breadcrumb 'Reports', reports_path
+  end
+
+  def datatables
+    add_breadcrumb 'Data Tables', datatables_path
+  end
 end
