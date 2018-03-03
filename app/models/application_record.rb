368c3c527ad46776e3d3af29bf86ad87179371da
Joshua Cox <jcox.pro@gmail.com>
2018-03-03 17:03:17 -0600

    check active

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,22 +2,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # instead of deleting, indicate the user requested a delete & timestamp it
-  def toggle_employee
-    if !deleted_at
-      update_attribute(:deleted_at, Time.current)
-    else
-      update_attribute(:deleted_at, nil)
-    end
-  end
-
-  # ensure user account is active
-  def active_for_authentication?
-    super && !deleted_at
-  end
-
-  # provide a custom message for a deleted account
-  def inactive_message
-    !deleted_at ? super : :deleted_account
-  end
+  
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,23 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # instead of deleting, indicate the user requested a delete & timestamp it
+  def toggle_employee
+    if !deleted_at
+      update_attribute(:deleted_at, Time.current)
+    else
+      update_attribute(:deleted_at, nil)
+    end
+  end
+
+  # ensure user account is active
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  # provide a custom message for a deleted account
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
 end
