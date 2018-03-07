fd047cc80908f7008dd6a747a617af3e9131e2b1
Joshua Cox <jcox.pro@gmail.com>
2018-03-07 17:11:15 -0600

    spacing

--- app/assets/javascripts/sb-admin.js
+++ app/assets/javascripts/sb-admin.js
@@ -1,9 +1,12 @@
 (function($) {
+
   "use strict"; // Start of use strict
+
   // Configure tooltips for collapsed side navigation
   $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
     template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
   })
+
   // Toggle the side navigation
   $("#sidenavToggler").click(function(e) {
     e.preventDefault();
@@ -11,11 +14,13 @@
     $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
     $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
   });
+
   // Force the toggled class to be removed when a collapsible nav link is clicked
   $(".navbar-sidenav .nav-link-collapse").click(function(e) {
     e.preventDefault();
     $("body").removeClass("sidenav-toggled");
   });
+
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
     var e0 = e.originalEvent,
@@ -23,6 +28,7 @@
     this.scrollTop += (delta < 0 ? 1 : -1) * 30;
     e.preventDefault();
   });
+
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
@@ -32,8 +38,10 @@
       $('.scroll-to-top').fadeOut();
     }
   });
+
   // Configure tooltips globally
   $('[data-toggle="tooltip"]').tooltip()
+
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(event) {
     var $anchor = $(this);
@@ -42,4 +50,5 @@
     }, 1000, 'easeInOutExpo');
     event.preventDefault();
   });
+  
 })(jQuery); // End of use strict
