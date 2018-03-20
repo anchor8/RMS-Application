2ea5d812be66e1b6f506e3bb22d69fbd5f919626
Joshua Cox <jcox.pro@gmail.com>
2018-03-19 21:10:11 -0500

    order line changes

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,3 +1,3 @@
 class OrderLine < ApplicationRecord
-  belongs_to :order
+  has_many :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -219,7 +219,7 @@ Vendor.create([
                   Zipcode: '77001' }
               ])
 
-Order.create([
+Order.create!([
     {vendor_id: 1, order_status_id: 2, customer_id: 1, country_id: 1, employee_id: 1, payment_type_id: 2, ship_via_id: 1, order_line_id: 1, state_id: 45, ShippingNumber: '12345', CustomerName: 'Home Depot', PurchaseOrderNumber: '1', OrderDate: '2017-01-16 14:12:01', ShipOnDate: '2017-2-16 12:00:00', OrderTotal: '5412.5', StreetAddress1: '12832 W Tomball Highway', StreetAddress2: '', City: 'Tomball', Zipcode: '77377'   }
 
 
