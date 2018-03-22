67016050814fb14f78675dd5f95cfffec516fed3
Joshua Cox <jcox.pro@gmail.com>
2018-03-22 15:30:27 -0500

    move require csv

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,8 +2,6 @@ class Customer < ApplicationRecord
   belongs_to :state
   has_many :orders
 
-  require 'csv'
-
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Customer.create! row.to_hash
