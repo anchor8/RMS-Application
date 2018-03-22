f29a1751d882a23926be79e5c0cd23845e6e4fda
Joshua Cox <jcox.pro@gmail.com>
2018-03-22 18:11:03 -0500

    moved employee helpers out of application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 # Application Helper
 module ApplicationHelper
-  def resource_name
-    :employee
-  end
-
-  def resource
-    @resource ||= Employee.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:employee]
-  end
+  
 
 end
--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,3 +1,14 @@
 # Employees Helper
 module EmployeesHelper
+  def resource_name
+    :employee
+  end
+
+  def resource
+    @resource ||= Employee.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:employee]
+  end
 end
