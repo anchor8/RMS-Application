947733bda1ee0f129712bb381ea7dd48e8316f88
Joshua Cox <jcox.pro@gmail.com>
2018-03-22 19:13:22 -0500

    Remove employee methods from employee helper
    Added them to app helper

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,14 +1,4 @@
 # Employees Helper
 module EmployeesHelper
-  def resource_name
-    :employee
-  end
 
-  def resource
-    @resource ||= Employee.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:employee]
-  end
 end
