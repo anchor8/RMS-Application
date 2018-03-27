ddd54c5c6fc7d1f5bdd432dfcae36fe317db7170
Joshua Cox <jcox.pro@gmail.com>
2018-03-27 13:48:43 -0500

    Update disable buttons

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -6,10 +6,10 @@ class Vendor < ApplicationRecord
   def toggle_vendor
     if !deleted_at
       update_attribute(:deleted_at, Time.current)
-      update_attribute(:vendor_status, 1)
+      update_attribute(:vendor_status_id, 1)
     else
       update_attribute(:deleted_at, nil)
-      update_attribute(:vendor_status, 2)
+      update_attribute(:vendor_status_id, 2)
     end
   end
 
