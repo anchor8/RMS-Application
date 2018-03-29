035d2ac1b4fe4197ea75b33aae6d55d9fe276cc2
Joshua Cox <jcox.pro@gmail.com>
2018-03-28 23:12:47 -0500

    minor changes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,7 @@
 # Employees Controller
 class EmployeesController < Devise::RegistrationsController
   def generate_new_password_email
-  employee = Employee.find(params[:employee_id])
-  Employee.send_reset_password_instructions flash[:notice] = "Reset password instructions have been sent to #{user.email}."
+    @employee = Employee.find(params[:employee_id])
+    Employee.send_reset_password_instructions flash[:notice] = "Reset password instructions have been sent to #{user.email}."
   end
 end
