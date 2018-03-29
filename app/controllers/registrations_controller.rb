b27850b8359461d9701007f464d4653d5ace4829
Joshua Cox <jcox.pro@gmail.com>
2018-03-28 23:56:29 -0500

    added permitted fields for employee devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,21 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up(resource_name, resource)
     true
   end
+
+  def after_update_path_for(resource)
+    usermgmt_path
+  end
+
+  def after_sign_up_path_for(resource)
+    usermgmt_path
+  end
+
+  def sign_up_params
+    params.require(:employee).permit(:email, :fname, :lname, :phone, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:employee).permit(:email, :fname, :lname, :phone, :password, :password_confirmation, :current_password)
+  end
+
 end
