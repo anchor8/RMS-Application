ef33792b921535effe67f3f574882f87238dc50e
Joshua Cox <jcox.pro@gmail.com>
2018-03-30 16:39:21 -0500

    fix devises error redirection

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,53 @@ class RegistrationsController < Devise::RegistrationsController
   before_action only: [:new, :create]
   skip_before_action :require_no_authentication
 
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      redirect_to usermgmt_path
+    end
+  end
+
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      redirect_to usermgmt_path
+    end
+  end
+
   protected
 
   def sign_up(resource_name, resource)
@@ -18,11 +65,11 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    params.require(:employee).permit(:email, :fname, :lname, :phone, :password, :password_confirmation)
+    params.require(:employee).permit(:email, :fname, :lname, :employee_status_id, :phone, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:employee).permit(:email, :fname, :lname, :phone, :password, :password_confirmation, :current_password)
+    params.require(:employee).permit(:email, :fname, :lname, :employee_status_id, :phone, :password, :password_confirmation, :current_password)
   end
 
 end
