314108ded1c809c167c24c09b35185b16725e0eb
Joshua Cox <jcox.pro@gmail.com>
2018-03-29 22:57:52 -0500

    find orderlines and delete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -83,6 +83,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:vendor_id, :order_status_id, :customer_id, :country_id, :employee_id, :payment_type_id, :shipper_id, :order_line_id, :state_id, :shipping_number, :customer_name, :purchase_order_number, :order_date, :ship_date, :order_total, :street_address_1, :street_address_2, :city, :zip_code)
+      params.require(:order).permit(:vendor_id, :order_status_id, :customer_id, :country_id, :employee_id, :payment_type_id, :shipper_id, :state_id, :shipping_number, :customer_name, :purchase_order_number, :order_date, :ship_date, :order_total, :street_address_1, :street_address_2, :city, :zip_code)
     end
 end
