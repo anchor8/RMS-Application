496767ee9eb2209167195760bad97896e3dc3fba
Joshua Cox <33433290+Joshua-Cox@users.noreply.github.com>
2018-03-31 11:11:51 -0500

    validate state

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,6 +2,8 @@ class State < ApplicationRecord
   has_many :customers
   has_many :orders
   has_many :vendors
+  
+  validates :state_name, allow_null: false, presence: true
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
