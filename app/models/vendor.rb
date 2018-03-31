562a070896244795f66a6f15591fb784e6b81b4b
Joshua Cox <33433290+Joshua-Cox@users.noreply.github.com>
2018-03-31 11:16:04 -0500

    vendor validations

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,6 +2,20 @@ class Vendor < ApplicationRecord
   belongs_to :state
   belongs_to :vendor_status
   has_many :orders
+  
+  validates :contact_name, allow_null: false, presence: true
+  validates :job_title, allow_null: false, presence: true
+  validates :work_phone, allow_null: false, presence: true
+  validates :mobile, allow_null: false, presence: true
+  validates :fax, allow_null: true, presence: false
+  validates :vendor_email, allow_null: true, presence: false
+  validates :vendor_cc_email, allow_null: true, presence: false
+  validates :website, allow_null: true, presence: false
+  validates :street_address_1, allow_null: false, presence: true
+  validates :street_address_2, allow_null: true, presence: false
+  validates :city, allow_null: false, presence: true
+  validates :zip_code, allow_null: false, presence: true
+  validates :deleted_at, allow_null: true, presence: false
 
   def toggle_vendor
     if !deleted_at
