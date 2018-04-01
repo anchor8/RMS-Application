20d6fdb503a03791612ead6d17398acc87c836eb
Joshua Cox <jcox.pro@gmail.com>
2018-04-01 01:20:17 -0500

    upgrade regex

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,7 @@
 class Country < ApplicationRecord
   has_many :orders
 
-  validates :country_name, allow_blank: false, presence: true, format: { with: /\A^\s*[a-zA-Z,\s.&\/]+\s*$\z/, message: "Only letters, commas, or spaces for country name allowed"}
+  validates :country_name, allow_blank: false, presence: true, format: { with: /\A^\s*[a-zA-Z,\s'.&)(\/-]+\s*$\z/, message: "Only letters, commas, dashes, parenthesis, or spaces for country name allowed"}
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
