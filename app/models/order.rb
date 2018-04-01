5a4d81deba81057929364dabc98b52b7bc8a8da6
Joshua Cox <jcox.pro@gmail.com>
2018-04-01 18:24:37 -0500

    shipping number and ship date must exist if shipped

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,10 +11,10 @@ class Order < ApplicationRecord
 
   # Validations
   validates :order_date, allow_blank: false, presence: true
-  validates :shipping_number, allow_blank: true, presence: false, format: { with: /\A^[0-9]*\z/, message: "Only numbers for shipping number allowed"}
   validates :purchase_order_number, allow_blank: false, presence: true, format: { with: /\A^[0-9]*\z/, message: "Only numbers for purchase order number allowed"}
-  validates :ship_date, allow_blank: true, presence: false
   validates :order_total, allow_blank: false, presence: true, format: { with: /\A^\d{1,20}(\.\d{0,2})?$\z/, message: "Order total must be only digits and 2 precision, example: ( 11111.11 ) "}
+  validates :shipping_number, presence: true, unless: ->(order){order.order_status_id == 1}
+  validates :ship_date, presence: true, unless: ->(order){order.order_status_id == 1}
   validates :street_address_1, allow_blank: false, presence: true
   validates :street_address_2, allow_blank: true, presence: false
   validates :city, allow_blank: false, presence: true, format: { with: /\A^\s*[a-zA-Z,\s]+\s*$\z/, message: "Only letters, commas, or spaces for city allowed"}
@@ -51,5 +51,4 @@ class Order < ApplicationRecord
       Order.create! row.to_hash
     end
   end
-
 end
