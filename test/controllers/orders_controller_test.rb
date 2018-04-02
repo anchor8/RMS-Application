1f29557558a62f4b545ecc3f5c0997cc8507cca4
Joshua Cox <jcox.pro@gmail.com>
2018-04-01 19:16:47 -0500

    fix tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -17,7 +17,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create order" do
     assert_difference('Order.count') do
-      post orders_url, params: { order: { city: @order.city, country_id: @order.country_id, customer_id: @order.customer_id, customer_name: @order.customer_name, employee_id: @order.employee_id, order_date: @order.order_date, order_status_id: @order.order_status_id, order_total: @order.order_total, payment_type_id: @order.payment_type_id, purchase_order_number: @order.purchase_order_number, ship_date: @order.ship_date, shipper_id: @order.shipper_id, shipping_number: @order.shipping_number, state_id: @order.state_id, street_address_1: @order.street_address_1, street_address_2: @order.street_address_2, vendor_id: @order.vendor_id, zip_code: @order.zip_code } }
+      post orders_url, params: { order: { city: @order.city, country_id: @order.country_id, customer_id: @order.customer_id, employee_id: @order.employee_id, order_date: @order.order_date, order_status_id: @order.order_status_id, order_total: @order.order_total, payment_type_id: @order.payment_type_id, purchase_order_number: @order.purchase_order_number, ship_date: @order.ship_date, shipper_id: @order.shipper_id, shipping_number: @order.shipping_number, state_id: @order.state_id, street_address_1: @order.street_address_1, street_address_2: @order.street_address_2, vendor_id: @order.vendor_id, zip_code: @order.zip_code } }
     end
 
     assert_redirected_to order_url(Order.last)
@@ -34,7 +34,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update order" do
-    patch order_url(@order), params: { order: { city: @order.city, country_id: @order.country_id, customer_id: @order.customer_id, customer_name: @order.customer_name, employee_id: @order.employee_id, order_date: @order.order_date, order_total: @order.order_total, payment_type_id: @order.payment_type_id, purchase_order_number: @order.purchase_order_number, ship_date: @order.ship_date, shipper_id: @order.shipper_id, shipping_number: @order.shipping_number, state_id: @order.state_id, street_address_1: @order.street_address_1, street_address_2: @order.street_address_2, vendor_id: @order.vendor_id, zip_code: @order.zip_code } }
+    patch order_url(@order), params: { order: { city: @order.city, country_id: @order.country_id, customer_id: @order.customer_id, employee_id: @order.employee_id, order_date: @order.order_date, order_total: @order.order_total, payment_type_id: @order.payment_type_id, purchase_order_number: @order.purchase_order_number, ship_date: @order.ship_date, shipper_id: @order.shipper_id, shipping_number: @order.shipping_number, state_id: @order.state_id, street_address_1: @order.street_address_1, street_address_2: @order.street_address_2, vendor_id: @order.vendor_id, zip_code: @order.zip_code } }
     assert_redirected_to order_url(@order)
   end
 
