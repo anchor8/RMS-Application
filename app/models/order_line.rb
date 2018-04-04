351879621213ea85e4296c465ee396de585a2943
Joshua Cox <jcox.pro@gmail.com>
2018-04-04 17:22:26 -0500

    fixed

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,13 @@
 class OrderLine < ApplicationRecord
   belongs_to :product
   belongs_to :order, optional: true
+
+  after_save :calculate_order_total
+
+  private
+
+  def calculate_order_total
+    order_total = OrderLine.where('order_id = ?', order.id).sum(:price)
+    Order.find(order.id).update_column(:order_total, order_total)
+  end
 end
