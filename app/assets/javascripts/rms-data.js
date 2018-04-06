3702dc2656ad5716f5c44f35a879e94445638345
Joshua Cox <jcox.pro@gmail.com>
2018-04-06 14:18:20 -0500

    cant hide first product

--- app/assets/javascripts/rms-data.js
+++ app/assets/javascripts/rms-data.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
 
     $('form').on('click', '.remove_record', function(event) {
       $(this).prev('input[type=hidden]').val('1');
-      $(this).closest('tr').hide();
+      $(this).closest('tr:not(:first-child)').hide();
       return event.preventDefault();
     });
 
