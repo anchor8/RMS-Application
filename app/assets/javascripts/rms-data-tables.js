8be2a3162e074fbdcafab7e9abe78a028631eada
Joshua Cox <jcox.pro@gmail.com>
2018-04-10 20:49:31 -0500

    include status in customer info

--- app/assets/javascripts/rms-data-tables.js
+++ app/assets/javascripts/rms-data-tables.js
@@ -390,7 +390,7 @@ $(document).ready(function() {
     customertable.columns( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ] ).visible( true, false );
 
     // Set columns that are not contact info to not visible
-    customertable.columns( [ 0, 1, 2, 6, 14, 15, 16, 17, 18, 19 ] ).visible( false, false );
+    customertable.columns( [ 0, 1, 2, 14, 15, 16, 17, 18, 19 ] ).visible( false, false );
 
     // adjust column sizing and redraw
     customertable.columns.adjust().draw( false );
