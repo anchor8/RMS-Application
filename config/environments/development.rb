7b26f060d38677bb333b3ba843aafdced18ed639
Joshua Cox <jcox.pro@gmail.com>
2018-04-10 20:14:53 -0500

    revert development env changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,30 +4,27 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  # if Rails.root.join('tmp/caching-dev.txt').exist?
-  #   config.action_controller.perform_caching = true
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
 
-  #   config.cache_store = :memory_store
-  #   config.public_file_server.headers = {
-  #     'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-  #   }
-  # else
-  #   config.action_controller.perform_caching = false
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
 
-  #   config.cache_store = :null_store
-  # end
-
-  config.cache_store = :memory_store
-  config.action_controller.perform_caching = true
+    config.cache_store = :null_store
+  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -41,7 +38,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  # config.assets.debug = true
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
